d5d9800eb0aadd91e718c00d85cad94610cabff4
slattsveen <slattsveen@gmail.com>
2017-01-20 14:50:51 +0000

    added keyboardshortcut to close window, fullscreen option and system command to activate vl42 driver for picam, would be better to have a better solution for the driver setup and avoid using system commands in the script

--- piCam_v01.py
+++ piCam_v01.py
@@ -1,9 +1,15 @@
-import sys, pygame, pygame.camera
+import sys, pygame, pygame.camera, os
+# to add the piCam to the list of available cameras, run command:
+# sudo modprobe bcm2835-v4l2
 
+os.system("sudo modprobe bcm2835-v4l2")
 pygame.init()
 pygame.camera.init()
 
+color1 = (128, 0, 128)
+color2 = (0, 0, 0)
 screen = pygame.display.set_mode((1280, 720), 0)
+#screen = pygame.display.set_mode((1280,720), pygame.FULLSCREEN)
 cam_list = pygame.camera.list_cameras()
 cam = pygame.camera.Camera(cam_list[0], (900,600))
 cam.start()
@@ -15,7 +21,11 @@ while True:
 	pygame.display.update()
 	
 	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
+		if event.type == pygame.QUIT or (event.type == pygame.KEYUP and event.key == pygame.K_ESCAPE):
 			cam.stop()
 			pygame.quit()
 			sys.exit()
+		if event.type == pygame.KEYUP and event.key == pygame.K_UP:
+			screen.fill(color1)
+		if event.type == pygame.KEYUP and event.key == pygame.K_DOWN:
+			screen.fill(color2)
