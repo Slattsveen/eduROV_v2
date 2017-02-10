3625e51dd6779e76a5b2a7ac990926ee4d63dc99
slattsveen <slattsveen@gmail.com>
2017-02-10 13:27:06 +0000

    the new file, v03_still.py, restarts the camera object and takes a high res picture during a low res stream

---
+++ piCam_v03_still.py
@@ -0,0 +1,44 @@
+import sys, pygame, pygame.camera, os, time
+# to add the piCam to the list of available cameras, run command:
+# sudo modprobe bcm2835-v4l2
+
+os.system("sudo modprobe bcm2835-v4l2")
+pygame.init()
+pygame.camera.init()
+FPS = 60
+fpsClock = pygame.time.Clock()
+imgCounter = 0
+
+color1 = (128, 0, 128)
+color2 = (0, 0, 0)
+#screen = pygame.display.set_mode((900, 600), 0)
+screen = pygame.display.set_mode((400,300), pygame.FULLSCREEN)
+cam_list = pygame.camera.list_cameras()
+cam = pygame.camera.Camera(cam_list[0], (400,300))
+cam.start()
+
+while True:
+	image1 = cam.get_image()
+	#image1 = pygame.transform.scale(image1, (900, 600))
+	#screen.blit(image1, (190, 60))
+	screen.blit(image1, (0, 0))
+	pygame.display.update()
+	fpsClock.tick(FPS)
+	
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT or (event.type == pygame.KEYUP and event.key == pygame.K_ESCAPE):
+			cam.stop()
+			pygame.quit()
+			sys.exit()
+		if event.type == pygame.KEYUP and event.key == pygame.K_UP:
+			cam.stop()
+			cam = pygame.camera.Camera(cam_list[0], (1024,768))
+			cam.start()
+			img = cam.get_image()
+			pygame.image.save(img, "pic_%s.jpg" %imgCounter)
+			imgCounter = imgCounter + 1
+			cam.stop()
+			cam = pygame.camera.Camera(cam_list[0], (400, 300))
+			cam.start()
+		if event.type == pygame.KEYUP and event.key == pygame.K_DOWN:
+			screen.fill(color2)
