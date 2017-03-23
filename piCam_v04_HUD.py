54d6f76cc8393b3bfd96e47d929d4483b307add3
slattsveen <slattsveen@gmail.com>
2017-03-23 17:21:22 +0000

    latest addition was made for testing various resolutions vs framerate for the VNC transfer

--- piCam_v04_HUD.py
+++ piCam_v04_HUD.py
@@ -21,11 +21,17 @@ cam_list = pygame.camera.list_cameras()
 cam = pygame.camera.Camera(cam_list[0], (picWidth,picHeight))
 cam.start()
 
+def texts(h, w):
+   font=pygame.font.Font(None,15)
+   scoretext=font.render(str(w) + " : " + str(h), 1,(255,255,255))
+   screen.blit(scoretext, (20, 20))
+
 while True:
 	image1 = cam.get_image()
 	image1 = pygame.transform.scale(image1, (900, 600))
 	#screen.blit(image1, (190, 60))
 	screen.blit(image1, (0, 0))
+	texts(picHeight, picWidth)
 	pygame.display.update()
 	fpsClock.tick(FPS)
 	
@@ -48,5 +54,13 @@ while True:
 			screen.fill(color2)
 		if event.type == pygame.KEYUP and event.key == pygame.K_LEFT:
 			picWidth -= 100
+			picHeight = int((picWidth*9)/16)
+			cam.stop()
+			cam = pygame.camera.Camera(cam_list[0], (picWidth, picHeight))
+			cam.start()
 		if event.type == pygame.KEYUP and event.key == pygame.K_RIGHT:
 			picWidth += 100
+			picHeight = int((picWidth*9)/16)
+			cam.stop()
+			cam = pygame.camera.Camera(cam_list[0], (picWidth, picHeight))
+			cam.start()
